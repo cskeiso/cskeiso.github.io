c2ee617f4bb54d790217424e8ee696db89daade7
Elliot Jenkins <elliotjenkins3@gmail.com>
2020-07-25 15:37:40 +1000

    moved work content over, CastingCouch page done

--- cskeiso/src/Routes.js
+++ cskeiso/src/Routes.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Home from './home/Home';
 import Work from './work/Work';
+import CastingCouch from './work/Casting_Couch/CastingCouch'
 import Text from './text/Text';
 import Audio from './audio/Audio';
 const routes = {
   '/': () => <Home />,
   '/text': () => <Text />,
   '/work': () => <Work />,
+  '/work/Casting-Couch': () => <CastingCouch />,
   '/audio': () => <Audio />
 };
 export default routes;
