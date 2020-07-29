5d607460add74392e81f18019bd6070ee5c41075
Elliot Jenkins <elliotjenkins3@gmail.com>
2020-07-29 20:49:11 +1000

    added files for gh-pages deploy

---
+++ precache-manifest.d0a4119416a703fa3405dbede087eb8c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f838d83608d1bf07221163668d01800",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10dd86da6d94aa10aa64",
+    "url": "/static/css/main.9aa89beb.chunk.css"
+  },
+  {
+    "revision": "e1e98b02fa38e460b7bf",
+    "url": "/static/js/2.deeb3b1b.chunk.js"
+  },
+  {
+    "revision": "7a29fd67914f99c562dc3f65bbf1078b",
+    "url": "/static/js/2.deeb3b1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10dd86da6d94aa10aa64",
+    "url": "/static/js/main.a85afce8.chunk.js"
+  },
+  {
+    "revision": "9ec56c775ec4d1623fdc",
+    "url": "/static/js/runtime-main.88d97200.js"
+  },
+  {
+    "revision": "b665499303f56c6790affaa0cc8fb879",
+    "url": "/static/media/CARMENKEISO_CV_03_21_2020.b6654993.pdf"
+  },
+  {
+    "revision": "6ad9f202cf50b7b112bbcabb52431064",
+    "url": "/static/media/art-almanac-hatched-pica.6ad9f202.png"
+  },
+  {
+    "revision": "bde80c6c0440786cfbfb0232c4dcb7a0",
+    "url": "/static/media/movie.bde80c6c.mp4"
+  },
+  {
+    "revision": "39091cf2349535efd289d87534138122",
+    "url": "/static/media/with-border.39091cf2.jpg"
+  }
+]);
